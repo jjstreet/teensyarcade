65a235cb4b9df0453a526e485cc0794a6917cbfb
Joshua Street <josh_s@ameritech.net>
2013-08-08 15:13:26 -0400

    Adding joystick support back (part 1)

---
+++ teensy/cores/usb_arcade/gamepad.h
@@ -0,0 +1,21 @@
+#ifndef GAMEPAD_H
+#define GAMEPAD_H
+
+// USB Gamepad Directional Keys
+#define D_RIGHT		0xFF
+#define D_LEFT		0x00
+#define D_UP		0xFF
+#define D_DOWN		0x00
+#define D_NONE		0x7F
+
+// USB Gamepad Buttons
+#define B_1			0x01
+#define B_2			0x02
+#define B_3			0x04
+#define B_4			0x08
+#define B_5			0x10
+#define B_6			0x20
+#define B_7			0x40
+#define B_8			0x80
+
+#endif
