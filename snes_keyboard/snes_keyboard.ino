7f86a1d13a39bca127dcfcd9ece09a5ad3fc9beb
Joshua Street <josh_s@ameritech.net>
2013-08-14 13:24:04 -0400

    Player 2 and fixes
    Added player 2 and fixed input pins to be INPUT_PULLUP

--- snes_keyboard/snes_keyboard.ino
+++ snes_keyboard/snes_keyboard.ino
@@ -2,92 +2,7 @@
  * SNES to Keyboard
  */
 
-const int PIN_CLOCK = 0;
-const int PIN_LATCH = 1;
-const int PIN_DATA = 2;
-
-const int dbTime = 10;
-
-// Controller Data
-int cData[16];
-
-// Read Controller Data
-int rData[16];
-
-// Count of data
-int cCount[16];
-
-void setup() {
-	// Setup pin assignments
-	pinMode(PIN_CLOCK, OUTPUT);
-	pinMode(PIN_LATCH, OUTPUT);
-	pinMode(PIN_DATA, INPUT);
-	
-	// Initialize clock and latch
-	digitalWrite(PIN_CLOCK, HIGH);
-	digitalWrite(PIN_LATCH, HIGH);
-}
-
-int time;
-
-void loop() {
-	if (millis() != time) {
-		readData();
-		processButtons();
-	}
-	time = millis();
-}
-
-void readData() {
-	digitalWrite(PIN_CLOCK, LOW);
-	digitalWrite(PIN_LATCH, LOW);
-	
-	digitalWrite(PIN_LATCH, HIGH);
-	delayMicroseconds(12);
-	digitalWrite(PIN_LATCH, LOW);
-	
-	// Cycle 0 - Available immediately
-	delayMicroseconds(6);
-	rData[0] = !digitalRead(PIN_DATA);
-	if (rData[0] == cData[0] && cCount[0] > 0) {
-		cCount[0]--;
-	}
-	if (rData[0] != cData[0]) {
-		cCount[0]++;
-	}
-	if (cCount[0] >= dbTime) {
-		cCount[0] = 0;
-		cData[0] = rData[0];
-	}
-	
-	// Cycles 1 - 15
-	for (int i = 1; i < 16; i++) {
-		digitalWrite(PIN_CLOCK, HIGH);
-		delayMicroseconds(6);
-		digitalWrite(PIN_CLOCK, LOW);
-		delayMicroseconds(6);
-		rData[i] = !digitalRead(PIN_DATA);
-		if (rData[i] == cData[i] && cCount[i] > 0) {
-			cCount[i]--;
-		}
-		if (rData[i] != cData[i]) {
-			cCount[i]++;
-		}
-		if (cCount[i] >= dbTime) {
-			cCount[i] = 0;
-			cData[i] = rData[i];
-		}
-	}
-}
-
-const int key_map[] = {
-	K_Z, K_A, K_V, K_F, K_UP, K_DOWN, K_LEFT, K_RIGHT, K_X, K_S, K_D, K_C
-};
-
-const int num_btns = 12;
-
 // Button Reference
-// Note controller is active low
 // 10000000 00000000 - B
 // 01000000 00000000 - Y
 // 00100000 00000000 - Select
@@ -101,15 +16,152 @@ const int num_btns = 12;
 // 00000000 00100000 - L
 // 00000000 00010000 - R
 
-void processButtons() {
+const int PIN_CLOCK = 12;
+const int PIN_LATCH = 14;
+const int PIN_DATA1 = 5;			// Player 1
+const int PIN_DATA2 = 16;			// Player 2
+
+// Debounce time
+const int dbTime = 10;
+
+// Controller Data
+int cData1[16];						// Player 1
+int cData2[16];						// Player 2
+
+// Read Controller Data
+int rData1[16];						// Player 1
+int rData2[16];						// Player 2
+
+// Counters for Controller Data
+int cCount1[16];					// Player 1
+int cCount2[16];					// Player 2
+
+// Timer
+int time;
+
+// Key maps for keyboard - See keycode.h
+const int keymap1[] = {				// Player 1
+	K_Q,								// B
+	K_W,								// Y
+	K_E,								// Select
+	K_R,								// Start
+	K_T,								// Up
+	K_Y,								// Down
+	K_U,								// Left
+	K_I,								// Right
+	K_O,								// A
+	K_P,								// X
+	K_A,								// L
+	K_S									// R
+};
+
+const int keymap2[] = {				// Player 2
+	K_D,								// B
+	K_F,								// Y
+	K_G,								// Select
+	K_H,								// Start
+	K_J,								// Up
+	K_K,								// Down
+	K_L,								// Left
+	K_Z,								// Right
+	K_X,								// A
+	K_C,								// X
+	K_V,								// L
+	K_B									// R
+};
+
+void setup() {
+	// Set pin assignments
+	// Pull up data pins
+	pinMode(PIN_CLOCK, OUTPUT);
+	pinMode(PIN_LATCH, OUTPUT);
+	pinMode(PIN_DATA1, INPUT_PULLUP);
+	pinMode(PIN_DATA2, INPUT_PULLUP);
+	
+	// Initialize clock and latch
+	digitalWrite(PIN_CLOCK, HIGH);
+	digitalWrite(PIN_LATCH, HIGH);
+}
+
+void loop() {
+	// Only process every 1ms
+	if (millis() != time) {
+		readControllers();
+		process();
+	}
+	time = millis();
+}
+
+
+void readControllers() {
 	int i;
-	for (i = 0; i < num_btns; i++) {
-		if (cData[i]) {
-			Keyboard.pressKey(key_map[i]);
+
+	// Reset controller read state
+	digitalWrite(PIN_CLOCK, LOW);
+	digitalWrite(PIN_LATCH, LOW);
+	
+	// Latch
+	digitalWrite(PIN_LATCH, HIGH);
+	delayMicroseconds(12);
+	digitalWrite(PIN_LATCH, LOW);
+	delayMicroseconds(6);
+	
+	// Cycle 0 - Available immediately
+	rData1[0] = !digitalRead(PIN_DATA1);
+	rData2[0] = !digitalRead(PIN_DATA2);
+	
+	// Cycles 1 - 15 - Pulse clock then read
+	for (i = 1; i < 16; i++) {
+		digitalWrite(PIN_CLOCK, HIGH);
+		delayMicroseconds(6);
+		digitalWrite(PIN_CLOCK, LOW);
+		delayMicroseconds(6);
+		rData1[i] = !digitalRead(PIN_DATA1);
+		rData2[i] = !digitalRead(PIN_DATA2);
+	}
+	
+	// Handle debounce
+	for (i = 0; i < 16; i++) {
+		// Player 1
+		if (rData1[i] == cData1[i] && cCount1[i] > 0) {
+			cCount1[i]--;
 		}
-		else {
-			Keyboard.releaseKey(key_map[i]);
+		if (rData1[i] != cData1[i]) {
+			cCount1[i]++;
+		}
+		if (cCount1[i] >= dbTime) {
+			cCount1[i] = 0;
+			cData1[i] = rData1[i];
+		}
+		// Player 2
+		if (rData2[i] == cData2[i] && cCount2[i] > 0) {
+			cCount2[i]--;
+		}
+		if (rData2[i] != cData2[i]) {
+			cCount2[i]++;
+		}
+		if (cCount2[i] >= dbTime) {
+			cCount2[i] = 0;
+			cData2[i] = rData2[i];
 		}
 	}
+}
+
+void process() {
+	// Set keys according to keymaps and button presses
+	int i;
+	for (i = 0; i < 12; i++) {
+		// Player 1
+		if (cData1[i])
+			Keyboard.pressKey(keymap1[i]);
+		else
+			Keyboard.releaseKey(keymap1[i]);
+		// Player 2
+		if (cData2[i])
+			Keyboard.pressKey(keymap2[i]);
+		else
+			Keyboard.releaseKey(keymap2[i]);
+	}
+	// Send keyboard report
 	Keyboard.send();
 }
